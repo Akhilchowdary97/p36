16d0958a3a54c2e307f73a418c63f56b9d3d7ae5
Akhilchowdary97 <akhilchowdary.m007@gmail.com>
2020-09-28 18:29:24 +0530

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from myapp import views
+app_name="myapp"
+urlpatterns = [
+    path('',views.home,name="home"),
+    path('register/',views.register,name='register'),
+    path('login/',views.user_login,name='user_login'),
+    path('logout/',views.user_logout,name='user_logout'),
+    path('profile/',views.profile,name="profile"),
+    path('reset_password/',views.reset_password,name="reset_password"),
+    path('frgt/',views.frgt_pwd,name="frgt"),
+    path('frgt/<id>',views.res_pwd,name="res_pwd"),
+]
